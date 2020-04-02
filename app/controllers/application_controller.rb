6833ff6faf88976ee27bb4663b323e99fb6013e2
Russell Macalintal <russell.macalintal@gmail.com>
2020-04-02 05:46:31 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -34,7 +34,7 @@ class ApplicationController < Sinatra::Base
 	post "/login" do
 		#your code here!
 		user = User.find_by(username: params[:username])
-		
+
 		if user
 			redirect '/success'
 		else
